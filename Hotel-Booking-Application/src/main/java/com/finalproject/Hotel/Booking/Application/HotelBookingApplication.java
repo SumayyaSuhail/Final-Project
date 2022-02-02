b4d578ca007a0f0631f74055b3df29d290c393f8
Sumayya Suhail <anzam.suhail4321@gmail.com>
2022-02-02 10:18:29 +0530

    Final Project begins..

---
+++ Hotel-Booking-Application/src/main/java/com/finalproject/Hotel/Booking/Application/HotelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.finalproject.Hotel.Booking.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingApplication.class, args);
+	}
+
+}
